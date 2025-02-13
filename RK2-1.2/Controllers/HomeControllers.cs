fd05060b8387a12e38da08352d6f63b84e585232
misakzltnc <k.m.f.a.z.1994@gmail.com>
2025-02-13 09:43:22 +0300

    Add files via upload

---
+++ RK2-1.2/Controllers/HomeControllers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldMvc.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return Content("Hello World");
+		}
+	}
+}
